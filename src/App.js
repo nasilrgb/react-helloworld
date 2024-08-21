fdb399073314f4535df789ea51cb5216c6bc7c2d
Nasil kp <nasilkp@Nasils-MacBook-Air.local>
2024-08-21 19:08:28 +0530

    changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          helloworld in stage branch
+          hello,world in stage branch 
         </p>
       </header>
     </div>
