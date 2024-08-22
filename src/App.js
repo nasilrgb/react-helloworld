d0198747f5a6e8835048b8e8b03beb436f3fd789
Nasil kp <nasilkp@Nasils-MacBook-Air.local>
2024-08-22 09:08:33 +0530

    added button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import logo from './logo.svg';
+
+// import React from 'react';
+// import './App.css';
+
+// function App() {
+//   const handleClick = () => {
+//     fetch('http://internal-k8s-csv3stag-devopste-a8c91d3b2d-1771359278.ap-south-1.elb.amazonaws.com/devops-testapp/service1')
+//       .then(response => response.text())  // Use .text() if the response is plain text
+//       .then(data => console.log(data))
+//       .catch(error => console.error('Error:', error));
+//   };
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Hello, World!</h1>
+//         <button onClick={handleClick}>Call ALB</button>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [responseText, setResponseText] = useState('');
+
+  const handleClick = () => {
+    fetch('http://internal-k8s-csv3stag-devopste-a8c91d3b2d-1771359278.ap-south-1.elb.amazonaws.com/devops-testapp/service1')
+      .then(response => response.text())  // Use .text() if the response is plain text
+      .then(data => setResponseText(data))
+      .catch(error => console.error('Error:', error));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          helloworld in stage branch 
-        </p>
+        <h1>Hello, World!</h1>
+        <button onClick={handleClick}>Call ALB</button>
+        <p>{responseText}</p> {/* Display the response here */}
       </header>
     </div>
   );
